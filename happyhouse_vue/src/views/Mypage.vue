8e3913a66c6b68bf7641853dea05b4b7e58a25e7
지수민 <wltn1873@gmail.com>
2021-11-21 00:07:33 +0900

    Add mypage

---
+++ happyhouse_vue/src/views/Mypage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header-nav />
+    <v-container class="text-center">
+      <h2>마이페이지</h2>
+    </v-container>
+    <div>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderNav from "../components/layout/HeaderNav.vue";
+export default {
+  name: "Mypage",
+  components: {
+    HeaderNav,
+  },
+};
+</script>
+
+<style></style>
